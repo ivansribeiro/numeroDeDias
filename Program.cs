216b33cb791e52843368da299f90adf241bfa3b0
Comelli <ivansrib@gmail.com>
2021-06-17 02:28:46 -0300

    Projeto Piloto

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exerc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nDias = 0;
+
+            Console.WriteLine("Digite o numero de meses :");
+            string meses = Console.ReadLine();
+            int intMeses = Convert.ToInt32(meses);
+
+            nDias = intMeses * 30;
+            Console.WriteLine("O numero de dias é:" + nDias);
+
+        }
+    }
+}
